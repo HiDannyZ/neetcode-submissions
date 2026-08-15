3b9169deef1ea8acda8ac635a675ea9dc0e6e915
Danny Z <chdannyzhu@yahoo.com>
2026-08-15 16:37:08 -0400

    Add: subsets - submission-6

---
+++ Data Structures & Algorithms/subsets/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # Decision tree of options 
+        # TC: O(2^n) because there are 2 decisions everytime and the factors never change
+        # Backtreking solution
+            # We want to explore every single combination
+        res = []
+        # Populate Res with the answer
+        self.dfs(0,[],nums,res)
+        return res
+
+    # Function: Get every single combination     
+    def dfs(self,index,currArray,nums,res):
+        if index == len(nums):
+            res.append(currArray.copy())
+            return
+
+        val = nums[index]
+
+        self.dfs(index+1,currArray,nums,res)
+        currArray.append(val)
+        self.dfs(index+1,currArray,nums,res)
+        currArray.pop()
+        # We decided to not add the val
